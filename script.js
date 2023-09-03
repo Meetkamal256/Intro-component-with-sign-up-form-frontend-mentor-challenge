b3c49224b5d241ba5f08b16a954eee57e16d1caa
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2023-09-04 00:42:04 +0100

    added media query for responsive design

---
+++ script.js
@@ -0,0 +1,59 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    
+    // get user input
+    const firstname = form['firstname'].value;
+    const lastname = form['lastname'].value;
+    const email = form['email'].value.trim();
+    const password = form['password'].value;
+    
+    console.log('First Name:', firstname);
+    console.log('Last Name:', lastname);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    
+    // check for empty inputs
+    if(firstname === ''){
+        addError('firstname', 'first name cannot be empty');
+    }else{
+        removeError('firstname');
+    }
+    if(lastname === ''){
+        addError('lastname', 'Last name cannot be empty');
+    }else{
+        removeError('lastname');
+    }
+    if(email === ''){
+        addError('email', 'email  cannot be empty');
+    }else if(!isValid('email')){
+        addError('email', 'Email is not valid');
+    }    
+    else{
+        removeError('email');
+    }
+    if(password === ''){
+        addError('password', 'password cannot be empty');
+    }else{
+        removeError('password');
+    }
+
+})
+
+
+function addError(field, message){
+    const formControl = form[field].parentNode;
+    formControl.classList.add('error');
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function removeError(field){
+    const formControl =form[field].parentNode;
+    formControl.classList.remove('error');
+}
+
+function isValid(email) {
+    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(email);
+  }
